eb7087c6b2e72b868eb2d0729bd2f08806033494
Shunta Komatsu <shuntak217@gmail.com>
2021-05-05 23:30:09 +0900

    refactor: apply black and isort and refactor main code

--- test_mse.py
+++ test_mse.py
@@ -1,85 +1,55 @@
-import os
+import os.path as osp
 import sys
-import numpy as np
-
-import glob
-import fnmatch
+from glob import glob
 
 import cv2
-
-from keras.models import Sequential
-from keras.layers import Dense
+import numpy as np
+import theano
 from keras.models import model_from_json
 
-#from model import VGG_16
-
-import theano
 theano.config.openmp = True
 
 
-from keras import backend as K
-def get_activations(model, layer, X_batch):
-    get_activations = K.function([model.layers[0].input, K.learning_phase()], model.layers[layer].output)
-    activations = get_activations([X_batch,0])
-    return activations
+BATCH_SIZE = 1
 
 
-def loadImages(folder):
-    imgs = []
-    matches = []
-    for root, dirnames, filenames in os.walk(folder):
-        for filename in fnmatch.filter(filenames, '*'):
-            matches.append(os.path.join(root, filename))
-   
-    return matches
+def load_model():
+    with open("./erika.json", "r") as f:
+        loaded_model_json = f.read()
 
-batch_size = 1
-
-
-def loadModel(home):
-# load json and create model
-    json_file = open('./erika.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
     model = model_from_json(loaded_model_json)
-    # load weights into new model
     model.load_weights("./erika_unstable.h5")
     return model
 
 
-def test(home):
-    model = loadModel(home)
-    for imname in loadImages(sys.argv[1]):
-        print(imname)
-        src = cv2.imread(imname,cv2.IMREAD_GRAYSCALE)
+def test():
+    model = load_model()
 
-        rows = int(src.shape[0]/16 + 1)*16
-        cols = int(src.shape[1]/16 + 1)*16
+    for path in sorted(glob(osp.join(sys.argv[1], "**", "*.jpg"))):
+        print(path)
 
-    
+        src = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        rows = int(src.shape[0] / 16 + 1) * 16
+        cols = int(src.shape[1] / 16 + 1) * 16
 
-        patch = np.empty((1,1,rows,cols),dtype="float32")
-        patch[0,0,:,:] = np.ones((rows,cols),dtype="float32")*255.0
-        patch[0,0,0:src.shape[0],0:src.shape[1]] = src
+        patch = np.empty((1, 1, rows, cols), dtype="float32")
+        patch[0, 0, :, :] = np.ones((rows, cols), dtype="float32") * 255.0
+        patch[0, 0, 0 : src.shape[0], 0 : src.shape[1]] = src
 
-        out = model.predict(patch, batch_size=batch_size)
+        out = model.predict(patch, batch_size=BATCH_SIZE)
         if isinstance(out, list):
             out = out[0]
 
-        result = np.zeros((rows,cols),dtype=np.float32)
+        result = np.zeros((rows, cols), dtype=np.float32)
+        result = out[0, 0, :, :]
+        result[result > 255] = 255
+        result[result < 0] = 0
 
-        result = out[0,0,:,:] 
-    
-        print(np.amax(result), np.amin(result))
+        cv2.imwrite(
+            osp.join(sys.argv[2], osp.basename(path)),
+            result[0 : src.shape[0], 0 : src.shape[1]],
+        )
 
-        result2 = cv2.normalize(result,0,255)
-
-        head, tail = os.path.split(imname)
-        #misc.imsave(sys.argv[2]+"/"+prefix+"_"+tail,result2[0:src.shape[0],0:src.shape[1]])
-
-        result[result>255] = 255
-        result[result<0] = 0
-        cv2.imwrite(sys.argv[2]+"/"+tail+".png",result[0:src.shape[0],0:src.shape[1]])
 
 if __name__ == "__main__":
-    test(sys.argv[1])
+    test()
